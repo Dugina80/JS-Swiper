9d2783a607852be06f4462a212c85bad127d5229
Dugina80 <alenka251280d@gmail.com>
2023-05-22 17:42:04 +0300

    added pagination

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-new Swiper('.image-slider',{
+/*new Swiper('.image-slider',{
     navigation: {
         nextE1: '.swiper-button-next',
         prevE1: '.swiper-button-prev'
     },
 });
+
+  let mySwiper = new Swiper('.image-slider', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });*/
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const mySwiper = new Swiper('.image-slider', {
+      //navigation: {
+        //nextEl: '.swiper-button-next',
+        //prevEl: '.swiper-button-prev',
+     // },  
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  });
+
